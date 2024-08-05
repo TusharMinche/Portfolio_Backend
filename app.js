42e0b06a1b5f31d16ee74fbeedd42ad482d81abd
TusharMinche <tusharminche@gmail.com>
2024-08-05 16:31:17 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,46 @@
+import cors from 'cors';
+import express, { json } from 'express';
+import 'dotenv/config';
+import nodemailer from 'nodemailer';
+
+const app = express();
+
+app.use(cors());
+app.use(json());
+
+const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
+    },
+});
+
+app.post("/send-email", async (req, res) => {
+    const { formData } = req.body;
+    try {
+        let mailOptions = {
+            from: formData.email,
+            to: "tusharminche@gmail.com",
+            subject: `Portfolio: ${formData.subject}`,
+            text: `Sender's Email: ${formData.email}\n\n${formData.message}`
+        };
+
+        await transporter.sendMail(mailOptions);
+        res.json({ message: "Message sent successfully." });
+    } catch (error) {
+        console.error('Error send message:', error);
+        res.status(500).json({ message: "Internal server error!" });
+    }
+});
+
+app.get('/', (req, res) => {
+    res.json({message: 'Working correctly'});
+})
+
+app.listen(5000, () => {
+    console.log("http://localhost:5000");
+});
